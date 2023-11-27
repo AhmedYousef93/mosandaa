24d923a428e362d633021b9245da0c3842a5bb25
kalashinkov <Ahmed.youssef1993@gmail.com>
2023-11-27 20:24:18 +0200

    update route confension

--- routes/admin.php
+++ routes/admin.php
@@ -1,51 +1,35 @@
 <?php
 
-use App\Http\Controllers\Admin\AboutController;
-use App\Http\Controllers\Admin\AdminController;
-use App\Http\Controllers\Admin\AdminsController;
-use App\Http\Controllers\Admin\CityController;
-use App\Http\Controllers\Admin\ContactController;
-use App\Http\Controllers\Admin\FaqsController;
-use App\Http\Controllers\Admin\ServiceController;
-use App\Http\Controllers\Admin\LoginController;
-use App\Http\Controllers\Admin\SubServiceController;
-use App\Http\Controllers\Admin\PrivecyController;
-use App\Http\Controllers\Admin\RelationsController;
-use App\Http\Controllers\Admin\RoleController;
-use App\Http\Controllers\Admin\SettingController;
-use App\Http\Controllers\Admin\TermsController;
-use App\Http\Controllers\Admin\UsageController;
-use App\Http\Controllers\Admin\UsersController;
 use Illuminate\Support\Facades\Route;
 
 
-        Route::get('admin/home',   [AdminController::class, 'index'])->name('adminhome');
-        Route::get('admin',        [LoginController::class, 'showLoginForm'])->name('admin.login');
-        Route::post('admin',       [LoginController::class, 'login']);
-        Route::get('lang/{local}', [AdminController::class, 'lang'])->name('lang');
+        Route::get('admin/home',   [\App\Http\Controllers\Admin\AdminController::class, 'index'])->name('adminhome');
+        Route::get('admin',        [\App\Http\Controllers\Admin\LoginController::class, 'showLoginForm'])->name('admin.login');
+        Route::post('admin',       [\App\Http\Controllers\Admin\LoginController::class, 'login']);
+        Route::get('lang/{local}', [\App\Http\Controllers\Admin\AdminController::class, 'lang'])->name('lang');
 
     Route::group(['prefix' => 'admin', 'middleware' => ['auth:admin']], function () {
 
-        Route::resource('roles',       RoleController::class)->except(['show']);
-        Route::resource('aboutus',     AboutController::class)->except(['show']);
-        Route::resource('contactus',   ContactController::class)->except(['create', 'edit']);
-        Route::resource('terms',       TermsController::class)->except(['show']);
-        Route::resource('usages',      UsageController::class)->except(['show']);
-        Route::resource('privecy',     PrivecyController::class)->except(['show']);
-        Route::resource('faqs',        FaqsController::class)->except(['show']);
-        Route::resource('users',       UsersController::class)->except(['show']);
-        Route::resource('admins',      AdminsController::class)->except(['show']);
-        Route::resource('cities',      CityController::class)->except(['show']);
-        Route::resource('services',    ServiceController::class)->except(['show']);
+        Route::resource('roles',       \App\Http\Controllers\Admin\RoleController::class)->except(['show']);
+        Route::resource('aboutus',     \App\Http\Controllers\Admin\AboutController::class)->except(['show']);
+        Route::resource('contactus',   \App\Http\Controllers\Admin\ContactController::class)->except(['create', 'edit']);
+        Route::resource('terms',       \App\Http\Controllers\Admin\TermsController::class)->except(['show']);
+        Route::resource('usages',      \App\Http\Controllers\Admin\UsageController::class)->except(['show']);
+        Route::resource('privecy',     \App\Http\Controllers\Admin\PrivecyController::class)->except(['show']);
+        Route::resource('faqs',        \App\Http\Controllers\Admin\FaqsController::class)->except(['show']);
+        Route::resource('users',       \App\Http\Controllers\Admin\UsersController::class)->except(['show']);
+        Route::resource('admins',      \App\Http\Controllers\Admin\AdminsController::class)->except(['show']);
+        Route::resource('cities',      \App\Http\Controllers\Admin\CityController::class)->except(['show']);
+        Route::resource('services',    \App\Http\Controllers\Admin\ServiceController::class)->except(['show']);
 
-        Route::get('/get-subservice-data/{subservice}', [SubServiceController::class, 'getSubserviceData']);
-        Route::put('/subservice/{subservice}', [SubserviceController::class, 'update']);
-        Route::get('orders/{id}',      [UsersController::class, 'sellerOrders'])->name('orderseller');
-        Route::get('setting',          [SettingController::class, 'index'])->name('setting');
-        Route::post('setting',         [SettingController::class, 'update'])->name('updatesetting');
-        Route::get('useractive',       [UsersController::class, 'UserStatus'])->name('useractive');
-        Route::get('adminactive',      [AdminsController::class, 'AdminStatus'])->name('adminactive');
-        Route::get('contactusDetail',  [ContactController::class, 'contactusDetails'])->name('contactusdetails');
-        Route::post('logout',          [LoginController::class, 'logout'])->name('admin.logout');
+        Route::get('/get-subservice-data/{subservice}', [\App\Http\Controllers\Admin\SubServiceController::class, 'getSubserviceData']);
+        Route::put('/subservice/{subservice}', [\App\Http\Controllers\Admin\SubserviceController::class, 'update']);
+        Route::get('orders/{id}',      [\App\Http\Controllers\Admin\UsersController::class, 'sellerOrders'])->name('orderseller');
+        Route::get('setting',          [\App\Http\Controllers\Admin\SettingController::class, 'index'])->name('setting');
+        Route::post('setting',         [\App\Http\Controllers\Admin\SettingController::class, 'update'])->name('updatesetting');
+        Route::get('useractive',       [\App\Http\Controllers\Admin\UsersController::class, 'UserStatus'])->name('useractive');
+        Route::get('adminactive',      [\App\Http\Controllers\Admin\AdminsController::class, 'AdminStatus'])->name('adminactive');
+        Route::get('contactusDetail',  [\App\Http\Controllers\Admin\ContactController::class, 'contactusDetails'])->name('contactusdetails');
+        Route::post('logout',          [\App\Http\Controllers\Admin\LoginController::class, 'logout'])->name('admin.logout');
         
     });
--- routes/api.php
+++ routes/api.php
@@ -1,45 +1,34 @@
 <?php
-
-use App\Http\Controllers\API\AccountController;
-use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\API\AuthDropdownController;
-use App\Http\Controllers\API\SettingController;
-use App\Http\Controllers\API\HomeKidController;
-use App\Http\Controllers\API\HomeSellerController;
-use App\Http\Controllers\API\NotificationController;
-use App\Http\Controllers\API\OrderController;
-use App\Http\Controllers\API\OrganizationController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 
 // authentication
-Route::post('register', [AuthController::class, 'register']); //register
-Route::post('verify', [AuthController::class, 'verify']); //verify
-Route::post('reset', [AuthController::class, 'resetCode']); //reset_code
-Route::post('resend', [AuthController::class, 'resendCode']); //resend_code
-Route::post('reset-password', [AuthController::class, 'resetPassword']); //reset password
-Route::post('login', [AuthController::class, 'Login']); //login
-Route::get('settings',         [SettingController::class,'index']); //relations
+Route::post('register', [\App\Http\Controllers\API\AuthController::class, 'register']); //register
+Route::post('verify', [\App\Http\Controllers\API\AuthController::class, 'verify']); //verify
+Route::post('reset', [\App\Http\Controllers\API\AuthController::class, 'resetCode']); //reset_code
+Route::post('resend', [\App\Http\Controllers\API\AuthController::class, 'resendCode']); //resend_code
+Route::post('reset-password', [\App\Http\Controllers\API\AuthController::class, 'resetPassword']); //reset password
+Route::post('login', [\App\Http\Controllers\API\AuthController::class, 'Login']); //login
+Route::get('settings',         [\App\Http\Controllers\API\SettingController::class,'index']); //relations
 
 // list of drops
-Route::get('cities', [SettingController::class, 'cities']); //cities
-Route::get('areas/{id}', [SettingController::class, 'areas']); //states
-Route::get('states/{id}', [SettingController::class, 'states']); //states
+Route::get('cities', [\App\Http\Controllers\API\SettingController::class, 'cities']); //cities
+Route::get('areas/{id}', [\App\Http\Controllers\API\SettingController::class, 'areas']); //states
+Route::get('states/{id}', [\App\Http\Controllers\API\SettingController::class, 'states']); //states
 
 
 Route::middleware(['auth:sanctum' , 'bindings'])->group( function () {
 
     // user setting
 
-    Route::get('logout', [AuthController::class, 'logout']); // logout
-    Route::post('user-update', [AuthController::class, 'update']); //update user
-    Route::get('user-profile', [AuthController::class, 'profile']); // user
-    Route::get('user-delete', [AuthController::class, 'delete']); //delete user
+    Route::get('logout', [\App\Http\Controllers\API\AuthController::class, 'logout']); // logout
+    Route::post('user-update', [\App\Http\Controllers\API\AuthController::class, 'update']); //update user
+    Route::get('user-profile', [\App\Http\Controllers\API\AuthController::class, 'profile']); // user
+    Route::get('user-delete', [\App\Http\Controllers\API\AuthController::class, 'delete']); //delete user
 
     
     
-    Route::get('notifications',         [NotificationController::class,'userNotifications']);
-    Route::post('delete-notification',  [NotificationController::class,'DeleteNotification']);
+    Route::get('notifications',         [\App\Http\Controllers\API\NotificationController::class,'userNotifications']);
+    Route::post('delete-notification',  [\App\Http\Controllers\API\NotificationController::class,'DeleteNotification']);
 });
