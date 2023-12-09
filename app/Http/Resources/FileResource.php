0227fb3995943a9a6a5b67346130ceb50b9619c6
ahmed yousef <ahmed.youssef1993@gmail.com>
2023-12-09 14:47:12 +0200

    resource file

--- app/Http/Resources/FileResource.php
+++ app/Http/Resources/FileResource.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class FileResource extends JsonResource
 {
-    public function toArray(Request $request): array
+    public function toArray($request)
     {
         return [
             'id' => (int) $this->id,
